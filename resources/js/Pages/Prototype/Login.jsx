ada5cee8679ca8e64d91cca6f5b8f5ec2b4ec08b
andi muh haikal lukman <113303662+itsdccal@users.noreply.github.com>
2024-07-14 20:48:44 +0800

    fix: fix issue with login

--- resources/js/Pages/Prototype/Login.jsx
+++ resources/js/Pages/Prototype/Login.jsx
@@ -34,7 +34,7 @@ export default function Login() {
                             </div>
                         </div>
                         <div className="grid space-y-[14px] mt-[30px]">
-                                <Link href={route('prototype.regster')}>
+                                <Link href={route('prototype.register')}>
                                     <Button type="button" variant="primary">
                                         <span className="text-base font-semibold">
                                             Start Watching
